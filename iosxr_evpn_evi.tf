2ad2653b0c54c874060cafbfad8bbb6e46b63b0d
rocrowe <rocrowe@cisco.com>
2025-10-21 15:41:17 -0400

    fixes

--- iosxr_evpn_evi.tf
+++ iosxr_evpn_evi.tf
@@ -1,72 +1,64 @@
 locals {
-  device_evpn_evis = flatten([
+  evpn_evis = flatten([
     for device in local.devices : [
-      for evi in try(local.device_config[device.name].evpn_evi, []) : {
+      for evi in try(local.device_config[device.name].evpn_evis, []) : {
         key                                 = "${device.name}-${evi.vpn_id}"
         device_name                         = device.name
-        vpn_id                              = try(evi.vpn_id, local.defaults.iosxr.configuration.evpn_evi.vpn_id, null)
-        description                         = try(evi.description, local.defaults.iosxr.configuration.evpn_evi.description, null)
-        load_balancing                      = try(evi.load_balancing, local.defaults.iosxr.configuration.evpn_evi.load_balancing, null)
-        load_balancing_flow_label_static    = try(evi.load_balancing_flow_label_static, local.defaults.iosxr.configuration.evpn_evi.load_balancing_flow_label_static, null)
-        bgp_rd_two_byte_as_number           = try(evi.bgp_rd_two_byte_as_number, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_two_byte_as_number, null)
-        bgp_rd_two_byte_as_assigned_number  = try(evi.bgp_rd_two_byte_as_assigned_number, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_two_byte_as_assigned_number, null)
-        bgp_rd_four_byte_as_number          = try(evi.bgp_rd_four_byte_as_number, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_four_byte_as_number, null)
-        bgp_rd_four_byte_as_assigned_number = try(evi.bgp_rd_four_byte_as_assigned_number, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_four_byte_as_assigned_number, null)
-        bgp_rd_ipv4_address                 = try(evi.bgp_rd_ipv4_address, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_ipv4_address, null)
-        bgp_rd_ipv4_address_assigned_number = try(evi.bgp_rd_ipv4_address_assigned_number, local.defaults.iosxr.configuration.evpn_evi.bgp_rd_ipv4_address_assigned_number, null)
-        bgp_route_policy_import             = try(evi.bgp_route_policy_import, local.defaults.iosxr.configuration.evpn_evi.bgp_route_policy_import, null)
-        bgp_route_policy_export             = try(evi.bgp_route_policy_export, local.defaults.iosxr.configuration.evpn_evi.bgp_route_policy_export, null)
-        advertise_mac                       = try(evi.advertise_mac, local.defaults.iosxr.configuration.evpn_evi.advertise_mac, null)
-        unknown_unicast_suppression         = try(evi.unknown_unicast_suppression, local.defaults.iosxr.configuration.evpn_evi.unknown_unicast_suppression, null)
-        control_word_disable                = try(evi.control_word_disable, local.defaults.iosxr.configuration.evpn_evi.control_word_disable, null)
-        etree                               = try(evi.etree, local.defaults.iosxr.configuration.evpn_evi.etree, null)
-        etree_leaf                          = try(evi.etree_leaf, local.defaults.iosxr.configuration.evpn_evi.etree_leaf, null)
-        etree_rt_leaf                       = try(evi.etree_rt_leaf, local.defaults.iosxr.configuration.evpn_evi.etree_rt_leaf, null)
-        bgp_route_target_import_two_byte_as_format = try(length(try(evi.bgp_route_target_import_two_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_two_byte_as_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_import_two_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_two_byte_as_format, []) : {
-            as_number       = try(rt.as_number, null)
-            assigned_number = try(rt.assigned_number, null)
+        vpn_id                              = try(evi.vpn_id, local.defaults.iosxr.configuration.evpn_evis.vpn_id, null)
+        description                         = try(evi.description, local.defaults.iosxr.configuration.evpn_evis.description, null)
+        load_balancing                      = try(evi.load_balancing, local.defaults.iosxr.configuration.evpn_evis.load_balancing, null)
+        load_balancing_flow_label_static    = try(evi.load_balancing_flow_label_static, local.defaults.iosxr.configuration.evpn_evis.load_balancing_flow_label_static, null)
+        bgp_rd_two_byte_as_number           = try(evi.bgp_rd_two_byte_as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_two_byte_as_number, null)
+        bgp_rd_two_byte_as_assigned_number  = try(evi.bgp_rd_two_byte_as_assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_two_byte_as_assigned_number, null)
+        bgp_rd_four_byte_as_number          = try(evi.bgp_rd_four_byte_as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_four_byte_as_number, null)
+        bgp_rd_four_byte_as_assigned_number = try(evi.bgp_rd_four_byte_as_assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_four_byte_as_assigned_number, null)
+        bgp_rd_ipv4_address                 = try(evi.bgp_rd_ipv4_address, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_ipv4_address, null)
+        bgp_rd_ipv4_address_assigned_number = try(evi.bgp_rd_ipv4_address_assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_rd_ipv4_address_assigned_number, null)
+        bgp_route_policy_import             = try(evi.bgp_route_policy_import, local.defaults.iosxr.configuration.evpn_evis.bgp_route_policy_import, null)
+        bgp_route_policy_export             = try(evi.bgp_route_policy_export, local.defaults.iosxr.configuration.evpn_evis.bgp_route_policy_export, null)
+        advertise_mac                       = try(evi.advertise_mac, local.defaults.iosxr.configuration.evpn_evis.advertise_mac, null)
+        unknown_unicast_suppression         = try(evi.unknown_unicast_suppression, local.defaults.iosxr.configuration.evpn_evis.unknown_unicast_suppression, null)
+        control_word_disable                = try(evi.control_word_disable, local.defaults.iosxr.configuration.evpn_evis.control_word_disable, null)
+        etree                               = try(evi.etree, local.defaults.iosxr.configuration.evpn_evis.etree, null)
+        etree_leaf                          = try(evi.etree_leaf, local.defaults.iosxr.configuration.evpn_evis.etree_leaf, null)
+        etree_rt_leaf                       = try(evi.etree_rt_leaf, local.defaults.iosxr.configuration.evpn_evis.etree_rt_leaf, null)
+        bgp_route_target_import_two_byte_as_format = try(length(evi.bgp_route_target_import_two_byte_as_format) == 0, true) ? null : [for rt in evi.bgp_route_target_import_two_byte_as_format : {
+          as_number       = try(rt.as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_two_byte_as_format.as_number, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_two_byte_as_format.assigned_number, null)
           }
         ]
-        bgp_route_target_import_four_byte_as_format = try(length(try(evi.bgp_route_target_import_four_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_four_byte_as_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_import_four_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_four_byte_as_format, []) : {
-            as_number       = try(rt.as_number, null)
-            assigned_number = try(rt.assigned_number, null)
+        bgp_route_target_import_four_byte_as_format = try(length(evi.bgp_route_target_import_four_byte_as_format) == 0, true) ? null : [for rt in evi.bgp_route_target_import_four_byte_as_format : {
+          as_number       = try(rt.as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_four_byte_as_format.as_number, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_four_byte_as_format.assigned_number, null)
           }
         ]
-        bgp_route_target_import_ipv4_address_format = try(length(try(evi.bgp_route_target_import_ipv4_address_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_ipv4_address_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_import_ipv4_address_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_import_ipv4_address_format, []) : {
-            ipv4_address    = try(rt.ipv4_address, null)
-            assigned_number = try(rt.assigned_number, null)
+        bgp_route_target_import_ipv4_address_format = try(length(evi.bgp_route_target_import_ipv4_address_format) == 0, true) ? null : [for rt in evi.bgp_route_target_import_ipv4_address_format : {
+          ipv4_address    = try(rt.ipv4_address, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_ipv4_address_format.ipv4_address, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_import_ipv4_address_format.assigned_number, null)
           }
         ]
-        bgp_route_target_export_two_byte_as_format = try(length(try(evi.bgp_route_target_export_two_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_two_byte_as_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_export_two_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_two_byte_as_format, []) : {
-            as_number       = try(rt.as_number, null)
-            assigned_number = try(rt.assigned_number, null)
+        bgp_route_target_export_two_byte_as_format = try(length(evi.bgp_route_target_export_two_byte_as_format) == 0, true) ? null : [for rt in evi.bgp_route_target_export_two_byte_as_format : {
+          as_number       = try(rt.as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_two_byte_as_format.as_number, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_two_byte_as_format.assigned_number, null)
           }
         ]
-        bgp_route_target_export_four_byte_as_format = try(length(try(evi.bgp_route_target_export_four_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_four_byte_as_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_export_four_byte_as_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_four_byte_as_format, []) : {
-            as_number       = try(rt.as_number, null)
-            assigned_number = try(rt.assigned_number, null)
+        bgp_route_target_export_four_byte_as_format = try(length(evi.bgp_route_target_export_four_byte_as_format) == 0, true) ? null : [for rt in evi.bgp_route_target_export_four_byte_as_format : {
+          as_number       = try(rt.as_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_four_byte_as_format.as_number, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_four_byte_as_format.assigned_number, null)
           }
         ]
-        bgp_route_target_export_ipv4_address_format = try(length(try(evi.bgp_route_target_export_ipv4_address_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_ipv4_address_format, [])) == 0, true) ? null : [
-          for rt in try(evi.bgp_route_target_export_ipv4_address_format, local.defaults.iosxr.configuration.evpn_evi.bgp_route_target_export_ipv4_address_format, []) : {
-            ipv4_address    = try(rt.ipv4_address, null)
-            assigned_number = try(rt.assigned_number, null)
+        bgp_route_target_export_ipv4_address_format = try(length(evi.bgp_route_target_export_ipv4_address_format) == 0, true) ? null : [for rt in evi.bgp_route_target_export_ipv4_address_format : {
+          ipv4_address    = try(rt.ipv4_address, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_ipv4_address_format.ipv4_address, null)
+          assigned_number = try(rt.assigned_number, local.defaults.iosxr.configuration.evpn_evis.bgp_route_target_export_ipv4_address_format.assigned_number, null)
           }
         ]
       }
-      if try(evi.vpn_id, local.defaults.iosxr.configuration.evpn_evi.vpn_id, null) != null
     ]
-    if try(local.device_config[device.name].evpn_evi, null) != null || try(local.defaults.iosxr.configuration.evpn_evi, null) != null
   ])
 }
 
 resource "iosxr_evpn_evi" "evpn_evi" {
-  for_each                                    = { for evi in local.device_evpn_evis : evi.key => evi }
+  for_each                                    = { for evi in local.evpn_evis : evi.key => evi }
   device                                      = each.value.device_name
   vpn_id                                      = each.value.vpn_id
   description                                 = each.value.description
